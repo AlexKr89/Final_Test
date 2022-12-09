b52113250cbef7dac4dde053ed308192b4186f2d
AlexKr <kaf-2012@mail.ru>
2022-12-09 20:42:06 +0300

    задача 2

---
+++ Сsharp/sem4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+
+int count = Counter(number);
+Console.Write(count);
+
+int Counter(int a)
+{
+    int count = 1;
+    while (a > 9)
+    {
+        a = a / 10;
+        count++;
+    }
+    return count;
+}
+
