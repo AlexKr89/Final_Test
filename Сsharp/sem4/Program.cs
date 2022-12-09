9249ca1f148ba31b03e03408df9ae4d16a26ad77
AlexKr <kaf-2012@mail.ru>
2022-12-09 20:24:41 +0300

    задача 1

--- Сsharp/sem4/Program.cs
+++ Сsharp/sem4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+ 
+int sumFromFunction = CalculateSum(number);
+Console.WriteLine(sumFromFunction);
+ 
+ 
+ 
+//<returnType> <functionName>(<param>)
+int CalculateSum(int lastNumber)
+{
+    int sum = 0;
+    for (int i = 1; i <= lastNumber; i++)
+    {
+        sum += i; // sum = sum + i
+    }
+    return sum;
+}
