ee90219ee72b0f678db4fb5433ab9441d6b436f3
AlexKr <kaf-2012@mail.ru>
2022-12-09 23:56:52 +0300

    27 comlete

--- Сsharp/seminar/project_drafts/Program.cs
+++ Сsharp/seminar/project_drafts/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sum = sumDigit(number);
+Console.WriteLine(sum);
+
+int sumDigit(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        int num = n % 10;
+        n = n / 10;
+        sum = sum + num;
+    }
+    return sum;
+}
