fc7adf7add8334f1d5b85afa1e2889386f5541af
AlexKr <kaf-2012@mail.ru>
2022-12-13 20:22:20 +0300

    task1 project выполнен

--- Сsharp/seminar/sem5/task1/Program.cs
+++ Сsharp/seminar/sem5/task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сумма всех цифр в введенном числе
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(DigitSum(number));
+
+int DigitSum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10; // sum = sum + num %10
+        num /= 10; //num = num/10
+    }
+    return sum;
+}
