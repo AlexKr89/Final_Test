78610055dad9cb3b0955a439a5930f23445b20e5
AlexKr <kaf-2012@mail.ru>
2022-12-13 20:33:02 +0300

    task1 project выполен

--- Сsharp/seminar/sem5/task1/Program.cs
+++ Сsharp/seminar/sem5/task1/Program.cs
@@ -1,17 +1,53 @@
-﻿// Сумма всех цифр в введенном числе
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
 
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine()!);
+int[] array = GenArray(12, -9, 9);
 
-Console.WriteLine(DigitSum(number));
+PrintArray(array);
+Console.WriteLine(PositiveSum(array));
+Console.WriteLine(NegativeSum(array));
 
-int DigitSum(int num)
+
+int[] GenArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+int PositiveSum(int[] array)
 {
     int sum = 0;
-    while (num > 0)
+    for (int i = 0; i < array.Length; i++)
     {
-        sum += num % 10; // sum = sum + num %10
-        num /= 10; //num = num/10
+        if (array[i] > 0)
+        {
+            sum += array[i];
+        }
     }
     return sum;
+}
+int NegativeSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
 }
