6445e70f70e0b56df3f83d53fc850a0b8f6718e5
AlexKr <kaf-2012@mail.ru>
2023-01-29 11:59:03 +0300

    Final_Test project done

--- Final_Test/Program.cs
+++ Final_Test/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Укажите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+string[] arr = UserFillArray(num);
+Console.WriteLine();
+PrintArray(arr);
+Console.WriteLine();
+int count = CountStringsNewArray(arr);
+if (count > 0)
+{
+    string[] arrNew = FillArrayNew(arr, count);
+    PrintArray(arrNew);
+}
+else Console.WriteLine("Не нашлось подходящих элементов");
+
+
+
+string[] UserFillArray(int number)
+{
+    string[] array = new string[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите строку");
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int CountStringsNewArray(string[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4) counter++;
+    }
+    return counter;
+}
+
+string[] FillArrayNew(string[] array, int counter)
+{
+    string[] arrayNew = new string[counter];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            arrayNew[j] = array[i];
+            j++;
+        }
+    }
+    return arrayNew;
+}
